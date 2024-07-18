370add54c546377e4b3c7dcf32b9123ec8f1fc7a
GabbySoft <gabrielnwagu2002@gmail.com>
2024-07-18 23:45:02 +0100

    Ready ti deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust this value as needed
+    outDir: "dist",
+  },
+});
